6f9b88380ba587d474f8938a82ab08ffc5c9e0c9
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-04-30 19:06:15 +0300

    add comments

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -3,34 +3,41 @@
  using namespace std;
 
 namespace ariel{
+    /*Constructors:*/ 
     Fraction::Fraction(): numerator(0), denominator(1){}
     
     Fraction::Fraction(int num , int den) : numerator(num), denominator(den) {
-        if(denominator == 0){
-            throw invalid_argument("denominator cant be zero");
+        if(denominator == 0){ // If denominator is zero- its illegal
+            throw invalid_argument("ERROR: denominator cant be zero");
         }
         this->reduce();
     }
     
-    //for it to fit the test
+    //needed for it to fit the test
     Fraction::Fraction(float number):numerator(1000*number),denominator(1000){
-           this->reduce();
+        // Convert the float to fraction by multiplying the numerator by 1000 and the denominator by 1000
+        // Then reduce the fraction
+        this->reduce();
     }
 
     // copy constructor
     // Fraction::Fraction(const Fraction& other) : numerator(other.numerator), denominator(other.denominator) {}
 
+    /*Reduce the fraction to its simplest form*/
     void Fraction::reduce() {
+        // Calculate the GCD of the numerator and denominator using the built-in __gcd() function
         int gcd = __gcd(abs(this->numerator), abs(this->denominator));
+        // Divide both numerator and denominator by the GCD to get the simplified fraction
         this->numerator /= gcd;
         this->denominator /= gcd;
+        // If the denominator is negative, change the signs of both numerator and denominator
         if (this->denominator < 0) {
             this->numerator *= -1;
             this->denominator *= -1;
         }
     }
 
-    //getters
+    /*getters:*/
     int Fraction::getNumerator() const{
         return this->numerator;
     }
@@ -40,16 +47,22 @@ namespace ariel{
 
     // Overloaded operators:
     Fraction operator+(const Fraction& num1, const Fraction& num2){
+        // Calculate the numerator and denominator of the sum.
         long long num = (long long)num1.getNumerator() * num2.getDenominator() + (long long)num2.getNumerator() * num1.getDenominator();
         long long den = (long long)num1.getDenominator() * num2.getDenominator();
+       
+        // Check for overflow by comparing with the minimum and maximum values of an int
+        int min_val = numeric_limits<int>::min();
+        int max_val = numeric_limits<int>::max();
 
-        if (num > (long long)numeric_limits<int>::max() || num < (long long)numeric_limits<int>::min())
+
+        if (num > max_val || num < min_val)
         {
-            throw overflow_error("overflow occurred during addition");
+            throw overflow_error("ERROR: overflow occurred during addition");
         }
-        if (den > (long long)numeric_limits<int>::max() || den < (long long)numeric_limits<int>::min())
+        if (den > max_val || den < min_val)
         {
-            throw overflow_error("overflow occurred during addition");
+            throw overflow_error("ERROR: overflow occurred during addition");
         }
 
         Fraction result((int)num, (int)den);
@@ -57,16 +70,21 @@ namespace ariel{
     }
 
     Fraction operator-(const Fraction& num1, const Fraction& num2){
+        // Calculate the numerator and denominator of the sub.
         long long num = (long long)num1.getNumerator() * num2.getDenominator() - (long long)num2.getNumerator() * num1.getDenominator();
         long long den = (long long)num1.getDenominator() * num2.getDenominator();
         
-        if (num > (long long)numeric_limits<int>::max() || num < (long long)numeric_limits<int>::min())
+        // Check for overflow by comparing with the minimum and maximum values of an int
+        int min_val = numeric_limits<int>::min();
+        int max_val = numeric_limits<int>::max();
+
+        if (num > max_val || num < min_val)
         {
-            throw overflow_error("overflow occurred during addition");
+            throw overflow_error("ERROR: overflow occurred during addition");
         }
-        if (den > (long long)numeric_limits<int>::max() || den < (long long)numeric_limits<int>::min())
+        if (den > max_val || den < min_val)
         {
-            throw overflow_error("overflow occurred during addition");
+            throw overflow_error("ERROR: overflow occurred during addition");
         }
 
         Fraction result((int)num, (int)den);
@@ -74,70 +92,70 @@ namespace ariel{
         return result;
     }
 
-Fraction operator*(const Fraction& num1, const Fraction& num2) {
-    if (num1.getNumerator() == 0 || num2.getNumerator() == 0) {
-        return Fraction(0);
+    Fraction operator*(const Fraction& num1, const Fraction& num2) {
+        // If any of the fractions is 0, then the answer is 0
+        if (num1.getNumerator() == 0 || num2.getNumerator() == 0) {
+            return Fraction(0);
+        }
+    
+        // Check for overflow when multiplying the numerator of the two fractions
+        // If one of the numerators is the minimum integer value, then multiplication will cause overflow
+        int min_val = numeric_limits<int>::min();
+
+
+        if (num1.getNumerator() == min_val || num2.getNumerator() == min_val) {
+            throw overflow_error("ERROR: overflow occurred during multiplication");
+        }
+        // Check for overflow when multiplying the two fractions' numerators and denominators
+        // Check if the numerator of one fraction is larger than the maximum integer value divided by the other fraction's denominator
+        // This means that when the numerators are multiplied, the result would be larger than the maximum integer value, causing overflow
+        int max_val = numeric_limits<int>::max();
+
+        if (num1.getNumerator() > max_val / num2.getDenominator() || num2.getNumerator() > max_val / num1.getDenominator()) {
+            throw overflow_error("ERROR: overflow occurred during multiplication");
+        }
+
+        // Calculate the numerator and denominator of the answer fraction
+        int num = num1.getNumerator() * num2.getNumerator();
+        int den = num1.getDenominator() * num2.getDenominator();
+
+        Fraction result(num, den);
+        result.reduce();
+        return result;
     }
-
-    int min_val = numeric_limits<int>::min();
-
-    if (num1.getNumerator() == min_val || num2.getNumerator() == min_val) {
-            throw overflow_error("overflow occurred during multiplication");
-    }
-
-    int max_val = numeric_limits<int>::max();
-
-    if (num1.getNumerator() > max_val / num2.getDenominator() || num2.getNumerator() > max_val / num1.getDenominator()) {
-        throw overflow_error("overflow occurred during multiplication");
-    }
-
-    int num = num1.getNumerator() * num2.getNumerator();
-    int den = num1.getDenominator() * num2.getDenominator();
-
-    Fraction result(num, den);
-    result.reduce();
-    return result;
-}
     
     Fraction operator/(const Fraction &num1, const Fraction &num2){
     
-    if (num2.getNumerator() == 0)
-    {
-        throw runtime_error("ilegal divison by 0");
-    }
+        if (num2.getNumerator() == 0){
+            throw runtime_error("ERROR: ilegal divison by 0");
+        }
 
-    long long num = (long long) num1.getNumerator() * (long long) num2.getDenominator(); 
-    long long den = (long long) num1.getDenominator() * (long long) num2.getNumerator();
+        long long num = (long long) num1.getNumerator() * (long long) num2.getDenominator(); 
+        long long den = (long long) num1.getDenominator() * (long long) num2.getNumerator();
     
-    int min_val = numeric_limits<int>::min();
-    int max_val = numeric_limits<int>::max();
+        int min_val = numeric_limits<int>::min();
+        int max_val = numeric_limits<int>::max();
 
-    if (num > max_val || num < min_val) {
-        throw overflow_error("Overflow occurred during division");
-    }
-    if (den > max_val || den < min_val) {
-        throw overflow_error("Overflow occurred during division");
-    }
+        if (num > max_val || num < min_val) {
+            throw overflow_error("ERROR: Overflow occurred during division");
+        }
+        if (den > max_val || den < min_val) {
+            throw overflow_error("ERROR: Overflow occurred during division");
+        }
 
-    Fraction result((int) num, (int) den);
-    return result;
+        Fraction result((int) num, (int) den);
+        return result;
 
     }   
 
 
     bool operator==(const Fraction& num1, const Fraction& num2){
-        // return (num1.numerator == num2.numerator) && (num1.denominator ==num2.denominator);
-
         // first multiplies the numerator of each fraction by 1000 and divides by the denominator, 
         // keeping only the three digits after the decimal point.
         int digits1 = (int)(num1.getNumerator() * 1000 / num1.getDenominator()) % 1000;
         int digits2 = (int)(num2.getNumerator() * 1000 / num2.getDenominator()) % 1000;
         return digits1 == digits2;
     }
-
-    // bool operator!=(const Fraction& num1, const Fraction& num2){
-    //     return !(num1 == num2);
-    // }
 
     bool operator!=(const Fraction& num1, const Fraction& num2) {
         
@@ -163,16 +181,16 @@ Fraction operator*(const Fraction& num1, const Fraction& num2) {
         return num1<num2 || num1==num2;
     }
 
-Fraction& Fraction::operator++(){ // prefix increment
-    if (numerator > numeric_limits<int>::max()) {
+    Fraction& Fraction::operator++(){ // prefix increment
+        if (numerator > numeric_limits<int>::max()) {
             throw overflow_error("Error: overflow occurred!") ;
-    } else {
-        numerator += denominator;
-        this->reduce();
-    }
+        } else {
+            numerator += denominator;
+            this->reduce();
+        }
 
-    return *this;
-}
+        return *this;
+    }
 
     const Fraction Fraction::operator++(int){ // postfix increment
         Fraction result = *this; //save it for return the object before the changes
